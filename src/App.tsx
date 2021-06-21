3fcee5b92a480ca17aeb4a60edbfe6a77c13091e
Harsh Patel <harsh.coderc@gmail.com>
2021-06-21 16:50:44 +0530

    Added Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import HomeScreen from "./screens/HomeScreen";
+
 const App = () => {
     return (
         <div>
-            <h1>Hello World!</h1>
+            <HomeScreen />
         </div>
     );
 };
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,7 @@
+import Container from "@material-ui/core/Container";
+
+const HomeScreen = () => {
+    return <Container maxWidth="md">Home Screen</Container>;
+};
+
+export default HomeScreen;
