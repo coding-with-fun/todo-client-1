7af5bc5dbf1a96e7b5066d0e04d651d3b7297807
Harsh Patel <harsh.coderc@gmail.com>
2021-06-21 19:19:08 +0530

    Added logic to toggle theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,8 @@ import { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 const Navbar = () => {
-    const { theme, handleToggleTheme } = useContext(ThemeContext);
+    const { handleToggleTheme } = useContext(ThemeContext);
 
-    console.log(theme);
     return (
         <div>
             <h1 onClick={handleToggleTheme}>Navbar</h1>
