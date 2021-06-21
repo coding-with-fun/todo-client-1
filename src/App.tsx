1067b5f50c89f73c1ccc43f2bc0a5a0f6a7d9229
Harsh Patel <harsh.coderc@gmail.com>
2021-06-21 21:05:23 +0530

    Added custom scss for theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import { ThemeProvider, Paper } from "@material-ui/core";
-import { createMuiTheme } from "@material-ui/core/styles";
-import { useContext } from "react";
 import Navbar from "./components/Navbar";
-import { ThemeContext } from "./contexts/ThemeContext";
 import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
-    const { theme } = useContext(ThemeContext);
-
-    const appliedTheme = createMuiTheme({
-        palette: {
-            type: theme === "light" ? "light" : "dark",
-        },
-    });
-
     return (
-        <ThemeProvider theme={appliedTheme}>
-            <Paper elevation={0} square className="h-screen">
-                <Navbar />
-                <HomeScreen />
-            </Paper>
-        </ThemeProvider>
+        <div>
+            <Navbar />
+            <HomeScreen />
+        </div>
     );
 };
 
