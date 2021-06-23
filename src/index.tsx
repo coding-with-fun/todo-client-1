27d0f7455a384cbb8435b8b450f64d5776620364
Harsh Patel <harsh.coderc@gmail.com>
2021-06-23 17:53:51 +0530

    Added analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
+import ReactGA from "react-ga";
 import ReactDOM from "react-dom";
 import App from "./App";
 import ThemeProvider from "./contexts/ThemeContext";
 import ToDoDataProvider from "./contexts/ToDoDataContext";
 import UserProvider from "./contexts/UserContext";
 import "./styles/index.scss";
+
+ReactGA.initialize("276811078"); // add your tracking id here.
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
     <ThemeProvider>
